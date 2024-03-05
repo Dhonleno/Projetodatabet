3ac98a0f6fc6b940abfb82f463d0cfdcfadc9428
Dhonleno <dhonlenofreitas@hotmail.com>
2024-03-05 14:43:30 -0300

    Databet

---
+++ Meu site/Js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+    
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+});
